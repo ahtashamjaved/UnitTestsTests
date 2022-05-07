7a48f1678d505f94b2c1902ed01d0119d7e28a77
ahtashamjaved <30853827+ahtashamjaved@users.noreply.github.com>
2022-05-07 13:39:49 +0500

    First Commit

---
+++ BasicMathOperationsTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Tests
+{
+    [TestClass()]
+    public class BasicMathOperationsTests
+    {
+        [TestMethod()]
+        public void TestAdd()
+        {
+            // Assemble
+            BasicMathOperations basicMathOperation = new BasicMathOperations();
+            int expectedResult = 12;
+
+            // Act
+            int actualResult = basicMathOperation.Add(9, 3);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod()]
+        public void SubTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
